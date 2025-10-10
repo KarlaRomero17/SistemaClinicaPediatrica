484b07f5c0d2368f410e165b2047130f05e2226b
Lissette Romero <49798659+KarlaRomero17@users.noreply.github.com>
2025-10-09 20:27:51 -0600

    feat: Agregar controlador y rutas para la gestión de pacientes

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/sistema-clinica', {
+mongoose.connect('mongodb+srv://romerolissette17_db_user:1dpI1ccfbti08MBW@cluster0.spjbizb.mongodb.net/sistema-clinica', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-// Rutas
+// Rutas para auth
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
+
+// Rutas para paciente
+const pacienteRoutes = require('./routes/paciente');
+app.use('/api/pacientes', pacienteRoutes); 
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Servidor ejecutándose en el puerto ${port}`);
